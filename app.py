37e66042221a88e37510fab1b8dfcd3642edf689
RocketFan <kubal123.tomczak@gmail.com>
2022-01-31 20:28:19 +0100

    Add test performance method

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import psutil
 import utils
 import numpy as np
+import pandas as pd
 
 from threading import Thread
 
@@ -16,6 +17,8 @@ from PyQt5.QtCore import Qt, QRectF
 
 from icp import ICP
 from icp_asm import ICPAsm
+
+THREADS = [1, 2, 4, 8, 16, 32, 64]
 
 
 class MainWindow(QMainWindow):
@@ -28,10 +31,12 @@ class MainWindow(QMainWindow):
         self.init_chart()
         self.next_button()
         self.reset_button()
+        self.performance_button()
         self.config_section()
 
-        self.init_icp()
-
+        config = self.get_config_dict()
+        self.init_icp(config)
+        self.icp.visualize_2D_results(self.chart_view.chart())
 
     def init_chart(self):
         chart = QChart()
@@ -52,18 +57,25 @@ class MainWindow(QMainWindow):
         self.counter_label.setGeometry(220, 0, 100, 30)
 
     def next_button(self):
-        next_button = QPushButton(self)
-        next_button.setText("Next")
-        next_button.setGeometry(0, 0, 100, 30)
-        
-        next_button.clicked.connect(self.next)
+        button = QPushButton(self)
+        button.setText("Next")
+        button.setGeometry(0, 0, 100, 30)
+
+        button.clicked.connect(self.next)
 
     def reset_button(self):
-        reset_button = QPushButton(self)
-        reset_button.setText("Reset")
-        reset_button.setGeometry(110, 0, 100, 30)
+        button = QPushButton(self)
+        button.setText("Reset")
+        button.setGeometry(110, 0, 100, 30)
 
-        reset_button.clicked.connect(self.reset)
+        button.clicked.connect(self.reset)
+    
+    def performance_button(self):
+        button = QPushButton(self)
+        button.setText("Test performance")
+        button.setGeometry(500, 0, 100, 30)
+
+        button.clicked.connect(self.test_performance)
 
     def config_section(self):
         x = 400
@@ -77,7 +89,8 @@ class MainWindow(QMainWindow):
         self.input_angle.setText("60")
         label_angle = QLabel(self)
         label_angle.setText("Angle:")
-        label_angle.setGeometry(self.input_angle.x() - 55, self.input_angle.y(), 50, height)
+        label_angle.setGeometry(self.input_angle.x() -
+                                55, self.input_angle.y(), 50, height)
 
         self.input_x = QLineEdit(self)
         self.input_x.setGeometry(x, 100, width, height)
@@ -85,7 +98,8 @@ class MainWindow(QMainWindow):
         self.input_x.setText("100")
         label_x = QLabel(self)
         label_x.setText("X:")
-        label_x.setGeometry(self.input_x.x() - 55, self.input_x.y(), 50, height)
+        label_x.setGeometry(self.input_x.x() - 55,
+                            self.input_x.y(), 50, height)
 
         self.input_y = QLineEdit(self)
         self.input_y.setGeometry(x, 150, width, height)
@@ -93,20 +107,23 @@ class MainWindow(QMainWindow):
         self.input_y.setText("0")
         label_y = QLabel(self)
         label_y.setText("Y:")
-        label_y.setGeometry(self.input_y.x() - 55, self.input_y.y(), 50, height)
+        label_y.setGeometry(self.input_y.x() - 55,
+                            self.input_y.y(), 50, height)
 
         self.slider_threads = QSlider(Qt.Horizontal, self)
         self.slider_threads.setGeometry(x - 25, 200, width + 50, height)
         self.slider_threads.setMinimum(1)
-        self.slider_threads.setMaximum(61)
+        self.slider_threads.setMaximum(64)
         self.slider_threads.setSingleStep(1)
         self.slider_threads.setValue(psutil.cpu_count(logical=False))
         print("Cores: ", self.slider_threads.value())
 
         label_threads = QLabel(self)
         label_threads.setText(f"Threads: {self.slider_threads.value()}")
-        label_threads.setGeometry(self.slider_threads.x() + 10, self.slider_threads.y() + 20, 80, height)
-        self.slider_threads.valueChanged.connect(lambda _: label_threads.setText(f"Threads: {self.slider_threads.value()}"))
+        label_threads.setGeometry(self.slider_threads.x(
+        ) + 10, self.slider_threads.y() + 20, 80, height)
+        self.slider_threads.valueChanged.connect(
+            lambda _: label_threads.setText(f"Threads: {self.slider_threads.value()}"))
 
         self.checkbox_asm = QCheckBox(self)
         self.checkbox_asm.setText("Use ASM")
@@ -128,25 +145,45 @@ class MainWindow(QMainWindow):
         self.counter_label.setText(f"Iteration: {self.icp.iteration}")
 
     def reset(self):
-        self.init_icp()
+        config = self.get_config_dict()
+        self.init_icp(config)
+        self.icp.visualize_2D_results(self.chart_view.chart())
         self.counter_label.setText("Iteration: 0")
 
-    def init_icp(self):
+    def test_performance(self):
+        df = pd.DataFrame({"Type": ["Python", "Asm"]})
+
         config = self.get_config_dict()
-        
+
+        for threads in THREADS:
+            config["threads"] = threads
+            results = []
+
+            for is_asm in [False, True]:
+                config["is_asm"] = is_asm
+                self.init_icp(config)
+                start = time.time()
+                self.icp.match_point_to_point()
+                self.icp.solve_SVD()
+                end = time.time() - start
+                results.append(end)
+
+            df[str(threads)] = results
+            
+        print(df)
+
+    def init_icp(self, config):
         R = utils.get_2D_R(np.radians(config["angle"]))
         T = utils.get_2D_T(config["x"], config["y"])
-        func = utils.get_2D_func([0.00003, 0.0002, 0, 0.001, 1])
+        func = utils.get_2D_func([0.0000003, 0.000002, 0, 0.00001, 1])
 
-        pc_1 = np.array([[x, func(x), 1] for x in range(-40, 25, 1)])
+        pc_1 = np.array([[x, func(x), 1] for x in range(-200, 200, 3)])
         pc_2 = np.dot(np.dot(pc_1, R.transpose()), T.transpose())
 
         if(config["is_asm"]):
             self.icp = ICPAsm(pc_1[:, :2], pc_2[:, :2], config["threads"])
         else:
             self.icp = ICP(pc_1[:, :2], pc_2[:, :2], config["threads"])
-
-        self.icp.visualize_2D_results(self.chart_view.chart())
 
 
 def main():
